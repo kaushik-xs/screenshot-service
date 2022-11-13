ecc61e4a521f55a482861a481f754cc066e19c5c
Kaushik Sundar <kaushik@tm>
2022-11-13 09:42:52 +0530

    Update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,20 @@
-FROM ghcr.io/puppeteer/puppeteer:16.1.0
+FROM node:16
+
+# Install latest chrome dev package and fonts to support major charsets (Chinese, Japanese, Arabic, Hebrew, Thai and a few others)
+# Note: this installs the necessary libs to make the bundled version of Chromium that Puppeteer
+# installs, work.
+ENV CHROME_BIN="/usr/bin/google-chrome-stable" \
+    PUPPETEER_SKIP_CHROMIUM_DOWNLOAD="true"
+RUN apt-get update \
+    && apt-get install -y wget gnupg \
+    && wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - \
+    && sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' \
+    && apt-get update \
+    && apt-get install -y google-chrome-stable fonts-ipafont-gothic fonts-wqy-zenhei fonts-thai-tlwg fonts-khmeros fonts-kacst fonts-freefont-ttf libxss1 \
+      --no-install-recommends 
+
+# Create app directory
+WORKDIR /usr/src/app
 
 # Install app dependencies
 # A wildcard is used to ensure both package.json AND package-lock.json are copied
--- index.js
+++ index.js
@@ -10,31 +10,34 @@ const os = require("os");
 //     origin: ['https://chateleon.com', 'https://www.chateleon.com', 'http://localhost:3000'],
 //     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 //   }
- 
-  
+
+
 function base64_encode(file) {
-    // read binary data
-    var bitmap = fs.readFileSync(file);
-    // convert binary data to base64 encoded string
-    return new Buffer(bitmap).toString('base64');
+  // read binary data
+  var bitmap = fs.readFileSync(file);
+  // convert binary data to base64 encoded string
+  return new Buffer(bitmap).toString('base64');
 }
 
 app.get("/", (req, res) => res.send("Service up and running"))
 app.post('/generate', cors(), async (req, res) => {
-    const url = req.query.url;
-    let browser = await puppeteer.launch();
- 
-    let page = await browser.newPage();
-    // await page.setViewportSize({ width: 1280, height: 1080 });
-    await page.goto(url);
-    const epochTime = Date.now();
-    await page.screenshot({ path: `${os.tmpdir()}/example-${epochTime}.png` });
-    await page.close();
-    await browser.close();
-    var base64str = base64_encode(`${os.tmpdir()}/example-${epochTime}.png`);
-    res.json({
-        screenshot: base64str
-    });
+  const url = req.query.url;
+  let browser = await puppeteer.launch({
+    headless: true,
+    executablePath: process.env.CHROME_BIN || null,
+    args: ['--no-sandbox', '--headless', '--disable-gpu', '--disable-dev-shm-usage']
+  });
+
+  let page = await browser.newPage();
+  await page.goto(url);
+  const epochTime = Date.now();
+  await page.screenshot({ path: `${os.tmpdir()}/example-${epochTime}.png` });
+  await page.close();
+  await browser.close();
+  var base64str = base64_encode(`${os.tmpdir()}/example-${epochTime}.png`);
+  res.json({
+    screenshot: base64str
+  });
 })
 
 app.listen(port, () => {
